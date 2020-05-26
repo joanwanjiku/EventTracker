e060c863ad606b8af557fb3220ce59a97a23c75b
clintonoliera <clintonoliera391@gmail.com>
2020-05-26 18:24:31 +0300

    added a validate form function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+  });
   $("#signin").click(function(){
+    $("form#form2").hide();
   $("form#form1").toggle();
   })
   $("#signup").click(function(){
@@ -7,3 +11,10 @@ $(document).ready(function(){
     $("form#form2").toggle();
   });
 })
+function validateform(){
+  var name = document.getElementById("name").value
+  var password = document.getElementById("password").value
+  if(name =="" && password ==""){
+    alert("Fill out all the fields");
+  }
+}
