7b874d32d7e5592348b5552487fa68a651d59a6a
Joan_E <joanevans18@gmail.com>
2020-05-27 21:52:32 +0300

    collect form input on event page

--- js/mainEvent.js
+++ js/mainEvent.js
@@ -1,2 +1,10 @@
 let user = JSON.parse(sessionStorage.user)
 document.getElementById('personName').innerHTML = user.userName;
+$(document).ready(function() {
+    $('#eventForm').submit(function(e) {
+      var nameEvent = $('#nameEvent').val();
+      var dateEvent = $('#eventDate').val();
+
+    });
+    
+})
