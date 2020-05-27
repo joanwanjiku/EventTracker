90d7de56d7ec5804d2021c6b08556cd416999b7f
mbalanya <okobacharles@yahoo.co.uk>
2020-05-27 17:48:01 +0300

    add scrolling functionality using jquery~

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  
+  $(window).scroll(function() {
+    if($(window).scrollTop() > 60 ) {
+      $('.my-navbar').addClass('navbar-scroll');
+    } else {
+      $('.my-navbar').removeClass('navbar-scroll');
+    };
+  });
   $("form").submit(function(event){
     event.preventDefault();
   });
